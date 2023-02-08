9b48f88fe22bd5685750edf09cbf017033a8e748
dragana.bojic <job.dragana@gmail.com>
2023-02-08 23:49:23 +0100

    team

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 function filterGallery(category) {
-    7
     var gallery = document.getElementById('gallery');
     switch (category) {
         case 'all':
@@ -7,37 +6,63 @@ function filterGallery(category) {
                 `'i1 i2 i3 i3'
                 'i1 i5 i6 i7'
                 'i4 i5 i6 i8'`;
-            // gallery.style.gridTemplateColumns = '1fr 1fr 1fr 1fr';
-            // gallery.style.gridTemplateRows = '1fr 1fr 1fr';
             hideImages([]);
             break;
         case 'logo':
             gallery.style.gridTemplateAreas =
                 `'i1 i2'
                 'i1 i7'`;
-                // gallery.style.gridTemplateColumns = '1fr 1fr';
-                // gallery.style.gridTemplateRows = '1fr 1fr';
-                hideImages(['i3', 'i3', 'i4', 'i5', 'i6', 'i8']);
+            hideImages(['i3', 'i3', 'i4', 'i5', 'i6', 'i8']);
             break;
+
+        case 'web':
+            gallery.style.gridTemplateAreas =
+                `'i3 i3'
+                'i6 i4'
+                'i6 i4'`;
+            hideImages(['i1', 'i2', 'i5', 'i7', 'i8']);
+            break;
+
+        case 'dev':
+            gallery.style.gridTemplateAreas =
+                `'i5 i8'`;
+            hideImages(['i1', 'i2', 'i3', 'i4', 'i6', 'i7']);
+            break;    // 
 
     }
 
-    
+
     console.log(gallery.style, gallery.style.gridTemplateAreas);
 }
 
-function hideImages(images){
+function hideImages(images) {
     var allImages = ['i1', 'i2', 'i3', 'i4', 'i5', 'i6', 'i7', 'i8'];
-    allImages.forEach(image => {
+    for (var i = 0; i < allImages.length; i++) {
+        var image = allImages[i];
         var imageElement = document.getElementById(image);
         console.log(image, imageElement);
-       if(images.indexOf(image) > -1){
+        if (images.indexOf(image) > -1) {
             imageElement.style.visibility = 'hidden';
-       } else{
+        } else {
             imageElement.style.visibility = 'visible';
-       }
-    });
+        }
+    }
 }
 
-// grid-template-columns: 1fr 1fr 1fr 1fr;
-// grid-template-rows: 1fr 1fr 1fr;
+setCounter(document.getElementById('project-count'), 256);
+setCounter(document.getElementById('client-count'), 355);
+setCounter(document.getElementById('coffe-count'), 825);
+setCounter(document.getElementById('recognition-count'), 75);
+
+
+function setCounter(element, endValue) {
+    let startValue = 0;
+    let duration = (3000 / endValue);
+    let counter = setInterval(function () {
+        startValue += 1;
+        element.textContent = startValue;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration);
+}
